da420df95dd96ca66ce267e26715e9ad9b6e6b13
henrychris <henrychris2003@gmail.com>
2021-05-17 18:10:42 +0100

    break statement broke loop. removed it.

--- Classes/Menu.cs
+++ Classes/Menu.cs
@@ -12,28 +12,36 @@ namespace ConsoleApp.Classes
             var userClient = new TwitterClient(Credentials.consumerKey, Credentials.consumerSecret,
             Credentials.accessToken, Credentials.accessSecret);
             var user = await userClient.Users.GetAuthenticatedUserAsync();
-            Console.WriteLine($"Welcome, {user}");
-            Console.ReadKey();
+            Console.WriteLine($"Welcome, {user}.");
+            Console.WriteLine();
+            await displayMenu();
         }
 
         public async Task displayMenu()
         {
             Actions act = new Actions();
             string input = "";
+
+
             while (input.ToUpper() != "Q")
-            {   
+            {
                 System.Console.WriteLine("  MENU    ");
-                System.Console.WriteLine("  1. Tweet\n  2. Follow a user\n  3. View a users timeline\n  Use Q or another key to Quit");
-                System.Console.Write("Make a selection using 1-3: ");
-                input = (string) Console.ReadLine();
+                System.Console.WriteLine("  1. Tweet\n  2. Follow a user\n  3. View a users timeline\n  Use Q or another key to display menu");
+                System.Console.Write("  Make a selection using 1-3: ");
+                input = (string)Console.ReadLine();
+                System.Console.WriteLine();
 
                 if (input == "1")
                 {
                     await act.Tweet();
                 }
-                else 
+                if (input == "2")
                 {
-                    break;
+
+                }
+                if (input == "3")
+                {
+
                 }
 
             }
